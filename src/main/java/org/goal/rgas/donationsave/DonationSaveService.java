f5b1db08eda11ce429ab23fb409ad82994d44106
Sangjin Yu <jj04090@naver.com>
2020-11-23 01:33:09 +0900

    20/11/23 last commit

--- src/main/java/org/goal/rgas/donationsave/DonationSaveService.java
+++
@@ -1,8 +0,0 @@
-package org.goal.rgas.donationsave;
-
-import java.util.List;
-
-public interface DonationSaveService {
-	public List<DonationSave> donationSaveList(DonationSave danationSave);
-	public void donationSaveModify(DonationSave DonationSave);
-}
